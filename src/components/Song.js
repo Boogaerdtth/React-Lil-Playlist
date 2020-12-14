cb7d4e2416b9db11f4eeb869f8825b1a173512a4
Jasper Boogaerdt t Hooft <jasper17@live.nl>
2020-12-14 08:05:42 +0100

    display tablecontent the right way

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,20 @@
-import React from 'react'
+// import React from 'react'
 
-function Song(props) {
-    return (
-        <div className="song">
-            <h3>{props.title} {props.artist} {props.genre} {props.rating} </h3>
-        </div>
-    )
-}
+// function Song(props) {
+//     return (
+//         <table className="song">
+//             <tbody>
+//                 <tr>
+//                     <th><h3>{props.title}</h3></th>
+//                     <th><h3>{props.artist}</h3></th>
+//                     <th><h3>{props.genre}</h3></th>
+//                     <th><h3>{props.rating}</h3></th>
 
-export default Song
+//                     {/* <h3>{props.title} {props.artist} {props.genre} {props.rating} </h3> */}
+//                 </tr>
+//             </tbody>
+//         </table>
+//     )
+// }
+
+// export default Song
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,37 +1,25 @@
 import React from 'react'
-import Song from './Song'
 
 function SongList(props) {
     return (
         <div className="list">
             <table className="song-header">
-                <thead>
-                    <tr>
-                        <th className="song-row__item">Song</th>
-                        <th className="song-row__item">Artist</th>
-                        <th className="song-row__item">Genre</th>
-                        <th className="song-row__item">Rating</th>
+                <th className="song-row__item">Song</th>
+                <th className="song-row__item">Artist</th>
+                <th className="song-row__item">Genre</th>
+                <th className="song-row__item">Rating</th>
+                {props.songs.map((song, id) => (
+                    <tr key={id} className="tableSongs">
+                        <td>{song.title}</td>
+                        <td>{song.artist}</td>
+                        <td>{song.genre}</td>
+                        <td>{song.rating}</td>
+                        {/* <td><button className="btnDelete" onClick={() => this.handleDelete(song._id)}></button></td> */}
                     </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>
-
-                            {props.songs.map(song =>
-                                <Song
-                                    key={song.id}
-                                    title={song.title}
-                                    artist={song.artist}
-                                    genre={song.genre}
-                                    rating={song.rating}
-                                />)}
-
-                        </th>
-                    </tr>
-                </tbody>
+                ))
+                }
             </table>
         </div >
     )
 }
-
 export default SongList
